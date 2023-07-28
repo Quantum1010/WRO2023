1b1b13dac91c8fedb4980fe1c5fd85c9ab838f7d
Quantum1010 <135439556+Quantum1010@users.noreply.github.com>
2023-07-29 01:09:37 +0900

    これで完成

--- 本番前チェック.py
+++ 本番前チェック.py
@@ -6,7 +6,6 @@ from pybricks.parameters import Port, Stop, Direction, Button, Color
 from pybricks.tools import wait, StopWatch, DataLog
 from pybricks.robotics import DriveBase
 from pybricks.media.ev3dev import SoundFile, ImageFile
-import uselect
 
 
 # This program requires LEGO EV3 MicroPython v2.0 or higher.
@@ -15,36 +14,24 @@ import uselect
 
 # Create your objects here.
 ev3 = EV3Brick()
-
-#ここからプログラムを書き込む
-ev3.speaker.beep()
-
-#インスタンスの作成
-color = ColorSensor(Port.S2)
-
+color_sensor=ColorSensor(Port.S2)
 left_motor = Motor(Port.B)
 right_motor = Motor(Port.C)
 arm = Motor(Port.A)
 
 #詳細
 wheel_diameter = 56
-axle_track = 150
+axle_track = 120
 robot = DriveBase(left_motor, right_motor, wheel_diameter, axle_track)
 
-#drive_timeの後の括弧はスピード(mm/s),ハンドルの角度？(deg/s),速度(ミリ秒つまり秒数×1000)
-
-robot.drive_time(100,0,2000)
+# Write your program here.
 robot.drive_time(0,90,1000)
-robot.drive_time(106,0,10000)
 robot.drive_time(0,-90,1000)
-robot.drive_time(100,0,4600)
-arm.on_for_degrees(50,90)
-arm.on_for_degrees(50,-90)
 robot.drive_time(0,180,1000)
-robot.drive_time(100,0,4600)
-robot.drive_time(0,90,1000)
-robot.drive_time(106,0,10000)
-robot.drive_time(0,90,1000)
-robot.drive_time(100,0,500)
-arm.on_for_degrees(50,90)
-arm.on_for_degrees(50,-90)
+robot.drive_time(0,-90,1000)
+
+while True:
+    robot.drive(200,0)
+    print(color_sensor.color())
+    if color_sensor.color == Color.BLACK:
+        ev3.speaker.beep()
